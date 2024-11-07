ee327bd0f902fba9e986a174f371d417d7841784
Joe Pietrzyk <joseph_pietrzyk@progressive.com>
2024-11-07 13:18:27 -0500

    Add routes for books

---
+++ src/app/books/books.component.ts
@@ -0,0 +1,10 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [],
+  template: ``,
+  styles: ``,
+})
+export class BooksComponent {}
